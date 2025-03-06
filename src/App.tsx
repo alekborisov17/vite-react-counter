f09f886e3be123576e7b706f233c1a0088fe9625
Alek Borisov <alekborisov@airia.com>
2025-03-06 13:08:00 +0200

    remove some goodies

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,11 @@ import './App.css'
 function App() {
 
   return (
-    <div className="container flex items-center">
-      <div className="card">
-        <h1 className={'text-center'}>{'0'}</h1>
-        <div className={'grid grid-cols-2'}>
-          <button className={'button'} onClick={() => {}}>{"Increment"}</button>
-          <button className={'button'} onClick={() => {}}>{"Decrement"}</button>
-        </div>
+      <div>
+        <h1>{'0'}</h1>
+        <button className={'button'} onClick={() => {}}>{"Increment"}</button>
+        <button className={'button'} onClick={() => {}}>{"Decrement"}</button>
       </div>
-    </div>
   )
 }
 
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,17 +1,11 @@
 
 import { create } from 'zustand'
 
-interface CounterState {
-  value: number
-  increment: () => void
-  decrement: () => void
-}
-
-// TODO: implement increment and decrement actions
-const useCounterStore = create<CounterState>((set) => ({
+// TODO: implement increment and decrement actions and add type
+const useCounterStore = create<any>((set) => ({
   value: 0,
-  increment: () => set((state) => state),
-  decrement: () => set((state) => state)
+  increment: () => set((state: any) => state),
+  decrement: () => set((state: any) => state)
 }))
 
 export default useCounterStore
