ed5c3e299992d891990c5e0b6a439b21c85adced
Alek Borisov <alekborisov@airia.com>
2025-03-19 13:14:29 +0200

    add url task

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import './App.css'
 
 function App() {
-
   return (
       <div>
         <h1>{'0'}</h1>
         <button className={'button'} onClick={() => {}}>{"Increment"}</button>
         <button className={'button'} onClick={() => {}}>{"Decrement"}</button>
+
+        <h2>User Data</h2>
+          <p>
+            <strong>Name:</strong>
+          </p>
+          <p>
+            <strong>Website:</strong>
+          </p>
+          <p>
+            <strong>Email:</strong>
+          </p>
+          <p>
+            <strong>Phone:</strong>
+          </p>
       </div>
   )
 }
